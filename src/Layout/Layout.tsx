5dcede0e847344372e9ad1261a89972caea39389
ypreiser <ypreiser@gmail.com>
2024-08-23 06:52:34 +0300

    nav

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+// src\Layout\Layout.tsx
+
+import { Outlet } from 'react-router-dom';
+// import NavBar from '../NavBar/';
+import style from './style.module.css';
+
+const Layout = () => {
+
+  return (
+    <div className={style.appContainer}>
+      {/* <NavBar /> */}
+      <main className={style.mainContent}>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
